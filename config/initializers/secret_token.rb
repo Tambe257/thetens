4f271cf03f5f74e186f46441bbd34828b7013c83
Tony Tambe <tambe257@gmail.com>
2013-04-27 08:11:41 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thetens::Application.config.secret_token = 'aac046bf6d2d856eb8c6a383d511cae6a1834ddd2a3e6f7081b8786bff8b79f3a0060a419a42b01a15f30027e37a398a05251db5430c7a1cbd58eed62f42c108'
