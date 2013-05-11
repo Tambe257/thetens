04f6a7ddfc9cae7237e2fffb19de27ab97933c22
Tony Tambe <tambe257@gmail.com>
2013-05-11 11:07:20 -0400

    Added pagination to show page

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -17,7 +17,8 @@ class PinsController < ApplicationController
   # GET /pins/1.json
   def show
     @pin = Pin.find(params[:id])
-
+    @pins = @pins.paginate(:page => params[:page], :per_page => 20)
+    
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @pin }
