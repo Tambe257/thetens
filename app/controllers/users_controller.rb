d04a1f10a49ce6b7139cb3e2059b0eb3787f87ba
Tony Tambe <tambe257@gmail.com>
2013-05-11 22:57:12 -0400

    Added more tens pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,41 @@ class UsersController < ApplicationController
     @pins = @user.pins.page(params[:page]).per_page(50)
 	end
 
+	def tens2000
+    @user = User.find(params[:id])
+    @pins = @user.pins.page(params[:page]).per_page(50)
+	end
+
+	def tens2001
+    @user = User.find(params[:id])
+    @pins = @user.pins.page(params[:page]).per_page(50)
+	end
+
+	def tens2002
+    @user = User.find(params[:id])
+    @pins = @user.pins.page(params[:page]).per_page(50)
+	end
+
+	def tens2003
+    @user = User.find(params[:id])
+    @pins = @user.pins.page(params[:page]).per_page(50)
+	end
+
+	def tens2004
+    @user = User.find(params[:id])
+    @pins = @user.pins.page(params[:page]).per_page(50)
+	end
+
+	def tens2005
+    @user = User.find(params[:id])
+    @pins = @user.pins.page(params[:page]).per_page(50)
+	end
+
+	def tens2006
+    @user = User.find(params[:id])
+    @pins = @user.pins.page(params[:page]).per_page(50)
+	end
+
 	def tens2007
     @user = User.find(params[:id])
     @pins = @user.pins.page(params[:page]).per_page(50)
--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,16 @@ Thetens::Application.routes.draw do
 
 
   devise_for :users
-  resources :users, only: [:show, :tens2007, :tens2008, :tens2009, :tens2010, :tens2011, :tens2013, :tens2012] do
+  resources :users, only: [:show, :tens2000, :tens2001, :tens2002, :tens2003, :tens2004, :tens2005, :tens2006, :tens2007, :tens2008, :tens2009, :tens2010, :tens2011, :tens2013, :tens2012] do
     member do
       get :show
+      get :tens2000
+      get :tens2001
+      get :tens2002
+      get :tens2003
+      get :tens2004
+      get :tens2005
+      get :tens2006
       get :tens2007
       get :tens2008
       get :tens2009
