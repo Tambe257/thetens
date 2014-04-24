4e357a212df0b89a36570b7138dbb344eb1da08b
Tony Tambe <tambe257@gmail.com>
2014-04-24 13:43:36 -0400

    changed create pin to redirect to root_path

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -51,7 +51,7 @@ class PinsController < ApplicationController
     respond_to do |format|
       if @pin.save
         MyMailer.post_alert(@pin).deliver
-        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Pin was successfully created.' }
         format.json { render json: @pin, status: :created, location: @pin }
       else
         format.html { render action: "new" }
