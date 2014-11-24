7062252b451735b507e522ad7f7b7eeb237ca2c4
Tony Tambe <tambe257@gmail.com>
2014-11-23 23:06:47 -0500

    Updated the stats page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,17 +7,18 @@ class PagesController < ApplicationController
     @users_total = User.count
     @pins = Pin.all
     @users = User.all
-    @popular_artist = Pin.count(:all, :group => 'artist', :order => 'count(*) DESC').first
-    @popular_album = Pin.count(:all, :group => 'album', :order => 'count(*) DESC').first
-    @no_1_album = Pin.where(:rank => '1').count(:all, :group => 'album', :order => 'count(*) DESC').first
+    #@popular_artist = Pin.count(:all, :group => 'artist', :order => 'count(*) DESC').first
+    @popular_artist = Pin.group(:artist).order('count_artist desc').count('artist').first
+    @popular_album = Pin.group(:album).order('count_album desc').count('album').first
+    @no_1_album = Pin.where(:rank => '1').group(:album).order('count_album desc').count('album').first
     @five_stars = Pin.where(:rating => '5').count
     @four_stars = Pin.where(:rating => '4').count
     @three_stars = Pin.where(:rating => '3').count
     @week = Pin.where('created_at >= ?', 1.week.ago.utc).count
-    @user_week_count = User.joins(:pins).where('pins.created_at >= ?', 1.week.ago.utc).count(:all, :group => 'name', :order => 'count(*) DESC').first
+    @user_week_count = User.joins(:pins).where('pins.created_at >= ?', 1.week.ago.utc).group(:name).order('count_name desc').count('name').first
     @user_week = User.joins(:pins).group('users.id').where('pins.created_at >= ?', 1.week.ago.utc).order('count(pins.id) desc').limit(1).first
     #@ave_rating = Pin.average('rating')
-    @rating_count = Pin.count(:all, :group => 'rating', :order => 'count(*) DESC').first
+    @rating_count = Pin.group(:rating).order('count_rating desc').count('rating').first
   end
 
   def reviewers
