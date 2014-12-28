c45066925c02eb8a10ff815bc728b8b4f644e3c8
Tony Tambe <tambe257@gmail.com>
2014-12-28 09:37:45 -0500

    Removed #{Rails}/vendor/assets/fonts

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,6 @@ module Thetens
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
-    config.assets.paths << "#{Rails}/vendor/assets/fonts"
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
     # Enable the asset pipeline
