b8e7c688825a02542162a1d5b070a6dc30408b50
Tony Tambe <tambe257@gmail.com>
2017-02-23 23:16:01 -0500

    adjusted the host in teh action_mailer section of teh production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Thetens::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # In production host should be updated to the actual host of your application
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'thetens.us' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
